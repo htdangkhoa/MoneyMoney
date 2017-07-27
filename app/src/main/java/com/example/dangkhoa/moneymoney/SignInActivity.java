19741326e8396b3d64d2ca3dc3866c9225fd27f7
DangKhoa <dangkhoa@DangKhoas-MacBook-Pro.local>
2017-07-27 18:03:51 +0700

    Commit Sign in page

---
+++ app/src/main/java/com/example/dangkhoa/moneymoney/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.example.dangkhoa.moneymoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        getSupportActionBar().hide();
+    }
+}
